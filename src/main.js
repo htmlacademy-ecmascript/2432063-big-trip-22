21735708df20ef241abe1e3663be0783b8f2cffc
EgorGER <QwertiQQQ@gmail.com>
2023-12-08 23:18:49 +0300

    созает модуль trip events item

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import tripFilter from './view/trip-filter.js';
 import {render} from './render.js';
+import tripEventItem from './view/trip-events-item.js';
 
 const pageHeader = document.querySelector('.page-header');
 const tripFilterControlPanel = pageHeader.querySelector('.trip-controls__filters');
 render(new tripFilter(), tripFilterControlPanel);
+
+const tripEvents = document.querySelector('.trip-events');
+for (let i = 1; i <= 3; i++) {
+  render (new tripEventItem(), tripEvents);
+}
