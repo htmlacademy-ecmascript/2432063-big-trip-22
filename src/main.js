dce70edcebf1ab0cdee783962d5a3129253db676
EgorGER <QwertiQQQ@gmail.com>
2023-12-11 18:53:16 +0300

    создает модуль event edit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import tripFilter from './view/trip-filter.js';
 import {render} from './render.js';
 import tripEventList from './view/trip-events-list.js';
 import sortListTrip from './view/sort-list-trip.js';
+import eventEdit from './view/event-edit.js';
 
 const pageHeader = document.querySelector('.page-header');
 const tripFilterControlPanel = pageHeader.querySelector('.trip-controls__filters');
@@ -9,6 +10,7 @@ render(new tripFilter(), tripFilterControlPanel);
 
 
 const tripEvents = document.querySelector('.trip-events');
+render (new eventEdit(), tripEvents);
 render (new sortListTrip(), tripEvents);
 
 for (let i = 1; i <= 3; i++) {
