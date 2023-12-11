697b8c79c375c304279595a6daa048cefd9fd5b9
EgorGER <QwertiQQQ@gmail.com>
2023-12-11 23:27:04 +0300

    фикс ошибок в пресенторе

--- src/main.js
+++ src/main.js
@@ -1,28 +1,26 @@
-/*import tripFilter from './view/trip-filter.js';
+import tripFilter from './view/trip-filter.js';
 import {render} from './render.js';
-import tripEventList from './view/trip-events-list.js';
-import sortListTrip from './view/sort-list-trip.js';
-import eventEdit from './view/event-edit.js';
+// import tripEventList from './view/trip-events-list.js';
+// import sortListTrip from './view/sort-list-trip.js';
+// import eventEdit from './view/event-edit.js';
 
 const pageHeader = document.querySelector('.page-header');
 const tripFilterControlPanel = pageHeader.querySelector('.trip-controls__filters');
 render(new tripFilter(), tripFilterControlPanel);
 
 
-const tripEvents = document.querySelector('.trip-events');
-render (new eventEdit(), tripEvents);
-render (new sortListTrip(), tripEvents);
+// const tripEvents = document.querySelector('.trip-events');
+// render (new eventEdit(), tripEvents);
+// render (new sortListTrip(), tripEvents);
 
-for (let i = 1; i <= 3; i++) {
-  render (new tripEventList(), tripEvents);
-}*/
+
+// }
 
 
 import BoardPresenter from './presenter/board-presenter.js';
 
 const tripEvents = document.querySelector('.trip-events');
 
-const boardPresenter = new BoardPresenter({ boardContainer: tripEvents});
+const boardPresenter = new BoardPresenter({boardContainer: tripEvents});
 
 boardPresenter.init();
-
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,13 +1,19 @@
-import {render} from './render.js';
-import sortListTrip from './view/sort-list-trip.js';
+import { render } from '../render.js';
+import tripEventList from '../view/trip-events-list.js';
+import sortListTrip from '../view/sort-list-trip.js';
+import eventEdit from '../view/event-edit.js';
 
 export default class BoardPresenter {
 
-  constructor({ boardContainer }) {
+  constructor({boardContainer}) {
     this.boardContainer = boardContainer;
   }
 
   init() {
-    render(new sortListTrip(), this.boardContainer);
+    render (new sortListTrip(), this.boardContainer);
+    render (new eventEdit(), this.boardContainer);
+    for (let i = 1; i <= 3; i++) {
+      render (new tripEventList(), this.boardContainer);
+    }
   }
 }
