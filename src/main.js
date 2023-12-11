7e24525d0de1551c99d7fd5cc565e9b58185d097
EgorGER <QwertiQQQ@gmail.com>
2023-12-11 18:35:09 +0300

    создает модуль sort-list-item

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import tripFilter from './view/trip-filter.js';
 import {render} from './render.js';
-import tripEventItem from './view/trip-events-item.js';
+import tripEventList from './view/trip-events-list.js';
+import sortListTrip from './view/sort-list-trip.js';
 
 const pageHeader = document.querySelector('.page-header');
 const tripFilterControlPanel = pageHeader.querySelector('.trip-controls__filters');
 render(new tripFilter(), tripFilterControlPanel);
 
+
 const tripEvents = document.querySelector('.trip-events');
+render (new sortListTrip(), tripEvents);
+
 for (let i = 1; i <= 3; i++) {
-  render (new tripEventItem(), tripEvents);
+  render (new tripEventList(), tripEvents);
 }
