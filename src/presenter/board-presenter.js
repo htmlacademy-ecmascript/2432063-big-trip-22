ed160fe6e34b49ca1b51f5ecd6f1105e5971c842
EgorGER <QwertiQQQ@gmail.com>
2024-01-03 21:08:17 +0300

    добовяет моковое данные в форму редактирования

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -18,7 +18,7 @@ export default class BoardPresenter {
     this.destinations = [...this.destinationsModel.getById()];
 
     render (new sortListTrip(), this.boardContainer);
-    render (new eventEdit(), this.boardContainer);
+    render (new eventEdit({destinations: this.destinations}), this.boardContainer);
 
     for (let i = 0; i <= this.liastPoints.length; i++) {
       render (new tripEventList({point: this.liastPoints[i], offer: this.offersModel, destinations: this.destinations}), this.boardContainer);
