42ad4bdbabd7dd0e05f35250fa1467bfade58136
EgorGER <QwertiQQQ@gmail.com>
2024-01-10 21:10:11 +0300

    добовдяет приватные поля в border-presenter

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,24 +4,30 @@ import sortListTrip from '../view/sort-list-trip.js';
 import eventEdit from '../view/event-edit.js';
 
 export default class BoardPresenter {
+  #boardContainer;
+  #liastPointsTripModel;
+  #offersModel;
+  #destinationsModel;
+  #liastPoints;
+  #destinations;
 
   constructor({boardContainer, listPointsTripModel, offersModel, destinationsModel}) {
-    this.boardContainer = boardContainer;
-    this.liastPointsTripModel = listPointsTripModel;
-    this.offersModel = offersModel;
-    this.destinationsModel = destinationsModel;
+    this.#boardContainer = boardContainer;
+    this.#liastPointsTripModel = listPointsTripModel;
+    this.#offersModel = offersModel;
+    this.#destinationsModel = destinationsModel;
   }
 
   init() {
-    this.liastPoints = [...this.liastPointsTripModel.points];
-    this.offersModel = [...this.offersModel.byType];
-    this.destinations = [...this.destinationsModel.byId];
+    this.#liastPoints = [...this.#liastPointsTripModel.points];
+    this.#offersModel = [...this.#offersModel.byType];
+    this.#destinations = [...this.#destinationsModel.byId];
 
-    render (new sortListTrip(), this.boardContainer);
-    render (new eventEdit({destinations: this.destinations}), this.boardContainer);
+    render (new sortListTrip(), this.#boardContainer);
+    render (new eventEdit({destinations: this.#destinations}), this.#boardContainer);
 
-    for (let i = 0; i <= this.liastPoints.length; i++) {
-      render (new tripEventList({point: this.liastPoints[i], offer: this.offersModel, destinations: this.destinations}), this.boardContainer);
+    for (let i = 0; i <= this.#liastPoints.length; i++) {
+      render (new tripEventList({point: this.#liastPoints[i], offer: this.#offersModel, destinations: this.#destinations}), this.#boardContainer);
     }
   }
 }
