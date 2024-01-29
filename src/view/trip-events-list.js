7143954118500d8cc0b05a678c8d6fed998caa6f
EgorGER <QwertiQQQ@gmail.com>
2024-01-29 18:59:31 +0300

    Добовляет слушатель для кнопки favorite

--- src/view/trip-events-list.js
+++ src/view/trip-events-list.js
@@ -4,7 +4,6 @@ import { getDuratiomAsString, getHoursFromString, getMinutesFromString, normaliz
 
 const renderTripEventItems = (destinations, point, offer) => {
   const {dateFrom, dateTo, basePrice, type, isFavorite, destination} = point;
-  const favorite = isFavorite ? 'active' : '';
 
   const getByType = (offerType) => offer.find(() => type === offerType);
   const getByDest = (offerType) => destinations.find(({ id }) => id === offerType);
@@ -42,7 +41,7 @@ const renderTripEventItems = (destinations, point, offer) => {
                     <span class="event__offer-price">${price}</span>
                   </li>
                 </ul>
-                <button class="event__favorite-btn event__favorite-btn--${favorite}" type="button">
+                <button class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
                   <span class="visually-hidden">Add to favorite</span>
                   <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
                     <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -63,16 +62,21 @@ export default class TripEventList extends AbstractView {
   #offer = null;
   #destinations = null;
   #handleClickPoint = null;
+  #favoriteClick = null;
 
-  constructor ({destinations, point, offer, onPointClick}){
+  constructor ({destinations, point, offer, onPointClick, onFavoriteClick}){
     super();
     this.#point = point;
     this.#offer = offer;
     this.#destinations = destinations;
     this.#handleClickPoint = onPointClick;
+    this.#favoriteClick = onFavoriteClick;
 
     this.element.querySelector('.event__rollup-btn')
       .addEventListener('click', this.#editClickPoint);
+
+    this.element.querySelector('.event__favorite-btn')
+      .addEventListener('click', this.#changeClickFavorite);
   }
 
   get template() {
@@ -82,5 +86,10 @@ export default class TripEventList extends AbstractView {
   #editClickPoint = (evt) => {
     evt.preventDefault();
     this.#handleClickPoint();
+  };
+
+  #changeClickFavorite = (evt) => {
+    evt.preventDefault();
+    this.#favoriteClick();
   };
 }
